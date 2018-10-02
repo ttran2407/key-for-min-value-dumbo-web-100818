bedac14bc8d954197ea3e09bb5110221b988d3bb
Tien Tran <tientran2407.vn@gmail.com>
2018-10-02 05:52:01 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+if name_hash == {} 
+   return nil 
+end
 
+min = { :random => 1000000000}
+name_hash.each do |key, value|
+  if min[:random] > value
+    min[:random] = value
+  end
+end
+
+name_hash.key (min[:random])
 end
